8388dd7bc114ba97415f1ed427bf04d20603905a
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-18 20:31:29 -0300

    feature_orders: create routes and controller methods

---
+++ BookstoreOrdersService/app/Http/Controllers/OrderController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class OrderController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * List all the user's orders;
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return JsonResponse
+     */
+    public function index(Request $request, int $id): JsonResponse
+    {
+        $orders = Order::all();
+
+        return $this->successResponse($orders);
+    }
+
+    /**
+     * Create an order for the user;
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $rules = [
+            'user_id' => 'required|min:1',
+            'book_id' => 'required|min:1',
+            'quantity' => 'required|min:1',
+        ];
+
+        $this->validate($request, $rules);
+
+        $order = Order::create($request->all());
+
+        return $this->successResponse($order, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Delete an user's order;
+     *
+     * @param Request $request
+     * @param integer $id
+     * @return JsonResponse
+     */
+    public function destroy(Request $request, int $id): JsonResponse
+    {
+        $order = Order::findOrFail($id);
+        $order->delete();
+
+        return $this->successResponse($order);
+    }
+}
