b22bbd0d70b38d2411306dd1c919598c05edd487
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-22 20:06:17 -0300

    feature_apigateway: guzzle config

---
+++ BookstoreApiGateway/config/services.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'books' => [
+        'base_uri' => env('BOOKS_SERVICE_BASE_URL'),
+        'secret' => env('BOOKS_SERVICE_SECRET'),
+    ],
+    'orders' => [
+        'base_uri' => env('ORDERS_SERVICE_BASE_URL'),
+        'secret' => env('ORDERS_SERVICE_SECRET'),
+    ],
+    'support' => [
+        'base_uri' => env('SUPPORT_SERVICE_BASE_URL'),
+        'secret' => env('SUPPORT_SERVICE_SECRET'),
+    ],
+    'search' => [
+        'base_uri' => env('SEARCH_SERVICE_BASE_URL'),
+        'secret' => env('SEARCH_SERVICE_SECRET'),
+    ],
+];
