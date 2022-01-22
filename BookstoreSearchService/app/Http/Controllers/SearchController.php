dd50381fb42d1f24a855e74303002e2c456b08b9
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-22 14:48:37 -0300

    feature_search: route, controller and standard responses

---
+++ BookstoreSearchService/app/Http/Controllers/SearchController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Traits\ApiResponser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Obtains and show a book by terms;
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function show(Request $request): JsonResponse
+    {
+        $term = $request->get('term');
+
+        $result = Book::where('title', 'like', "%{$term}%")
+                    ->orWhere('author', 'like', "%{$term}%")
+                    ->orWhere('category', 'like', "%{$term}%")
+                    ->get();
+
+        return $this->successResponse($result);
+    }
+}
