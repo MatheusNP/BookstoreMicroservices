4870fbb57951e7f0ec1f19c7c01f85be6dbba01e
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-22 20:06:49 -0300

    feature_apigateway: guzzle request method

---
+++ BookstoreApiGateway/app/Traits/ConsumesExternalService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Traits;
+
+use GuzzleHttp\Client;
+
+trait ConsumesExternalService
+{
+
+    /**
+     * Send a request to any service;
+     *
+     * @param string $method
+     * @param string $requestUrl
+     * @param array $formParams
+     * @param array $headers
+     * @return string
+     */
+    public function performRequest(
+        string $method,
+        string $requestUrl,
+        array $formParams = [],
+        array $query = [],
+        array $headers = []
+    ): string
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUri,
+        ]);
+
+        if (isset($this->secret)) {
+            $headers['Authorization'] = $this->secret;
+        }
+
+        $response = $client->request(
+            $method,
+            $requestUrl,
+            [
+                'form_params' => $formParams,
+                'query' => $query,
+                'headers' => $headers,
+            ]
+        );
+
+        return $response->getBody()->getContents();
+    }
+}
