03b5b3a4648a2fc5382864e8abf5618ea255270a
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-22 14:44:28 -0300

    feature_search: initial config

---
+++ BookstoreSearchService/database/migrations/2022_01_22_030000_create_books_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_id');
+            $table->string('title');
+            $table->string('author');
+            $table->text('description');
+            $table->string('publisher');
+            $table->string('category');
+            $table->string('language');
+            $table->integer('edition')->unsigned();
+            $table->integer('page')->unsigned();
+            $table->integer('weight')->unsigned();
+            $table->integer('maximum_price')->unsigned();
+            $table->integer('offered_price')->unsigned();
+            $table->integer('discount_pctg')->unsigned();
+            $table->integer('available')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
