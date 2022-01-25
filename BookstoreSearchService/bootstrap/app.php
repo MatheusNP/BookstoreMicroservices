a8a29989c2b9ba42e0de3aa06173b89b860579cc
Matheus Nobre <matheusnobre56@gmail.com>
2022-01-24 22:17:13 -0300

    feature_search: authenticate access secret

--- BookstoreSearchService/bootstrap/app.php
+++ BookstoreSearchService/bootstrap/app.php
@@ -73,9 +73,9 @@ $app->configure('queue');
 |
 */
 
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
+$app->middleware([
+    App\Http\Middleware\AuthenticateAccess::class
+]);
 
 // $app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
