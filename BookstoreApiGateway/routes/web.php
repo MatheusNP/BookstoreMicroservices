25a6fdd9eb25656ccdb31ce35c0191c4ce9e2f3d
Matheus Nobre <matheusnobre56@gmail.com>
2023-11-20 22:05:21 -0300

    refactor: remove trailing spaces

--- BookstoreApiGateway/routes/web.php
+++ BookstoreApiGateway/routes/web.php
@@ -15,30 +15,30 @@
 
 $router->group(['prefix' => 'api'], function() use ($router) {
 
-    $router->post('/users/login', 'UserController@login');    
+    $router->post('/users/login', 'UserController@login');
     $router->post('/users', 'UserController@store');
 
     $router->group(['middleware' => 'auth:api'], function () use ($router) {
         $router->get('/users/me', 'UserController@me');
-        $router->post('/users/logout', 'UserController@logout');    
+        $router->post('/users/logout', 'UserController@logout');
     });
 
     $router->group(['middleware' => 'client.credentials'], function() use ($router) {
 
         $router->get('/books', 'BookController@index');
-        $router->get('/books/category/', 'BookController@listCategory');    
-        $router->get('/books/author/', 'BookController@listAuthor');    
-        $router->get('/books/{product_id}', 'BookController@show');    
+        $router->get('/books/category/', 'BookController@listCategory');
+        $router->get('/books/author/', 'BookController@listAuthor');
+        $router->get('/books/{product_id}', 'BookController@show');
 
-        $router->get('/orders/me/', 'OrderController@list');    
-        $router->post('/orders', 'OrderController@store');    
-        $router->delete('/orders/{id}', 'OrderController@destroy');    
-        $router->delete('/orders/complete/me/', 'OrderController@complete');    
+        $router->get('/orders/me/', 'OrderController@list');
+        $router->post('/orders', 'OrderController@store');
+        $router->delete('/orders/{id}', 'OrderController@destroy');
+        $router->delete('/orders/complete/me/', 'OrderController@complete');
 
         $router->get('/tickets/me/', 'TicketController@list');
-        $router->post('/tickets', 'TicketController@store');    
+        $router->post('/tickets', 'TicketController@store');
 
-        $router->get('/search', 'SearchController@show');    
+        $router->get('/search', 'SearchController@show');
 
         $router->get('/mail', 'MailController@index');
     });
